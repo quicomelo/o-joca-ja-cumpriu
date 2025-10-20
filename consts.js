6e723e77266289736866f5c918eeac43424a1dc0
quicomelo <quicomelo@proton.me>
2025-10-20 10:52:34 +0000

    Update consts.js

--- consts.js
+++ consts.js
@@ -3,9 +3,8 @@ const PARTY = process.env.NEXT_PUBLIC_PARTY || process.env.PARTY;
 const REPO_URL = process.env.NEXT_PUBLIC_REPO_URL || process.env.REPO_URL;
 
 const PARTIES = {
-  ps: 'Partido Socialista',
-  ad: 'Aliança Democrática',
-}
+  psd: 'Partido Social Democrata',
+  }
 const PARTY_NAME = PARTIES[PARTY];
 export {
   PARTY,
