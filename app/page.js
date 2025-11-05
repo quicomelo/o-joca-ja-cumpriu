fe02e281c20a4dccb870c854c8dfe41663399f50
quicomelo <quicomelo@proton.me>
2025-11-05 23:34:42 +0000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 import { getSections } from '@/lib/promessas';
 import Section from '@/app/_components/section';
 import Cumpridometro from '@/app/_components/cumpridometro';
+import Image from 'next/image';
+import { getSections } from '@/lib/promessas';
 
 export default async function Home() {
   const sections = getSections();
@@ -8,6 +10,13 @@ export default async function Home() {
   return (
     <div>
       <div className='text-center mb-12'>
+      <Image
+          src="/imagens/presidente.png"
+          alt="Presidente da Câmara"
+          width={200}
+          height={200}
+          className="mx-auto rounded-full mb-4 shadow-lg"
+        />
         <h1 className='text-4xl mb-4'>O Joca Já Cumpriu?</h1>
         <Cumpridometro />
         <a className='underline' href="/programas/programa.pdf">Programa</a>
